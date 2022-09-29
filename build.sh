a1dd1644b8672f4f2334b31bd5e0ee78d5fbab76
OpenEtude <melaoufir@gmail.com>
2022-09-29 18:29:37 +0100

    Bugfix atempt

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 APP_VERSION=$(cat "application.properties" | grep app.version | cut -d "=" -f2-)
 TSTAMP=$(date +%Y%m%d%H%M%S%Z)
-docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.m2/:/root/.m2/ ghcr.io/openetude/grailsbuilder:main grails set-version ${APP_VERSION}.${GITHUB_RUN_ID}_${TSTAMP} && \
-docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.m2/:/root/.m2/ ghcr.io/openetude/grailsbuilder:main grails prod war
+docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.ivy2/:/root/.ivy2/ \
+    ghcr.io/openetude/grailsbuilder:1.3.8.1.1 /bin/bash -c \
+    "ls -altr /root/.ivy2/cache/ && ls -altr /root/.grails/ && grails set-version ${APP_VERSION}.${GITHUB_RUN_ID}_${TSTAMP} && grails prod war"
