cfd85dc3c7a6a001a4211b911c6191c8f273a745
OpenEtude <melaoufir@gmail.com>
2022-09-29 17:58:46 +0100

    Bugfix atempt

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-source /root/.sdkman/bin/sdkman-init.sh
 APP_VERSION=$(cat "application.properties" | grep app.version | cut -d "=" -f2-)
 TSTAMP=$(date +%Y%m%d%H%M%S%Z)
-yes | docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.m2/:/root/.m2/ ghcr.io/openetude/grailsbuilder:main grails upgrade && \
 docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.m2/:/root/.m2/ ghcr.io/openetude/grailsbuilder:main grails set-version ${APP_VERSION}.${GITHUB_RUN_ID}_${TSTAMP} && \
 docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.m2/:/root/.m2/ ghcr.io/openetude/grailsbuilder:main grails prod war
