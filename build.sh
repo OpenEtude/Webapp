b92db08b776a9821afb2ca7501227761d0ce1e00
OpenEtude <melaoufir@gmail.com>
2022-10-01 22:15:52 +0100

    Bugfix atempt

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ mkdir -p ${HOME}/.ivy2/cache/
 mkdir -p ${HOME}/.grails/
 docker run -it -v ${PWD}:/project -v ${HOME}/.grails/:/root/.grails/  -v ${HOME}/.ivy2/:/root/.ivy2/ \
     ghcr.io/openetude/grailsbuilder:1.3.8-1.2 /bin/bash -c \
-    "ls -altr /project/ && ls -altr /root/.grails/1.3.8/projects/project/stage/ && ls -altr /root/.grails/1.3.8/projects/project/stage/WEB-INF/ && grails set-version ${APP_VERSION}.${GITHUB_RUN_ID}_${TSTAMP} && grails prod war"
+    "grails set-version ${APP_VERSION}.${GITHUB_RUN_ID}_${TSTAMP} && grails prod war"
